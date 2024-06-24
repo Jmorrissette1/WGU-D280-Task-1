8d3681afbe9dd8cae47246018ed4bea49b27c24b
Jmorrissette1 <J.morrissette47@gmail.com>
2024-06-24 14:59:00 -0700

    Api is working, will refactor to make observable

--- src/app/worldmap/worldmap.component.ts
+++ src/app/worldmap/worldmap.component.ts
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './worldmap.component.css'
 })
 export class WorldmapComponent {
+  ngOnInit(): void {
+    let svgCountryPaths = document.querySelectorAll<SVGPathElement>('path');
 
+    Array.prototype.forEach.call(svgCountryPaths, (svgCountry: SVGPathElement) => {
+
+      svgCountry.addEventListener('mouseover', (event:MouseEvent)=> {
+        const path = event.target as SVGPathElement;
+        path.style.fill = 'green';
+      });
+
+      svgCountry.addEventListener('mouseleave', (event:MouseEvent)=> {
+        const path = event.target as SVGPathElement;
+        path.style.fill = '';
+      });
+      
+      svgCountry.addEventListener('click', () => {
+       this.loadCountryData(svgCountry);
+      });
+    });
+  }
+
+  async loadCountryData(svgCountry: SVGPathElement){
+    let api: string = 'https://api.worldbank.org/V2/country/'+svgCountry.id+'?format=json';
+    let response: Response = await fetch(api);
+    let data: any = await response.json();
+    let dataPath: any = data[1];
+
+    let name: string = dataPath[0].name;
+    document.getElementById('name')!.innerText = name;
+
+
+  }
 }
