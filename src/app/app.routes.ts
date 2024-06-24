11ec6a9eda3969bcb82280d284fdb1b7bfe481fc
Jmorrissette1 <J.morrissette47@gmail.com>
2024-06-24 12:09:25 -0700

    Routed Map, and have all Components talking to each other.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WorldmapComponent } from './worldmap/worldmap.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: WorldmapComponent},
+];
