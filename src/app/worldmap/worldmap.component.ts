a1cb73a1c322a5f1cc047e45e88b26a6733e7ed9
Jmorrissette1 <J.morrissette47@gmail.com>
2024-06-24 11:59:52 -0700

    started project

---
+++ src/app/worldmap/worldmap.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worldmap',
+  standalone: true,
+  imports: [],
+  templateUrl: './worldmap.component.html',
+  styleUrl: './worldmap.component.css'
+})
+export class WorldmapComponent {
+
+}
